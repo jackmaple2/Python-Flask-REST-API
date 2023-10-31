7f784a3634991e30366f515c53bf77d7de06a0e2
jackmaple2 <jackmaple3000@gmail.com>
2023-10-31 16:23:40 +0000

    adds dynamic name to url

--- myenv/name.py
+++ myenv/name.py
@@ -4,5 +4,11 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self, name):
+        return {'name': name}
+    
+api.add_resource(HelloWorld, '/helloworld/<string:name>/<int:test>')
+
 if __name__ == '__main__':
     app.run(debug=True)
