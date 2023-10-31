1147f79236e85dc1479294f0a2539e3c291c6887
jackmaple2 <jackmaple3000@gmail.com>
2023-10-31 17:04:01 +0000

    adds 404 error code to get request

--- myenv/test.py
+++ myenv/test.py
@@ -2,6 +2,9 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-response = requests.get(BASE + 'helloworld/bill')
+#response = requests.put(BASE + 'video/1', {'likes': 10, 'name': 'jack', 'views': 100})
+#print(response.json())
+#input()
+response = requests.get(BASE + 'video/6')
 print(response.json())
 
